23c2ddc2323723844ccd4aefc611fd2545a7b1a1
Hamidreza <www.hamidreza121212@gmail.com>
2024-09-26 14:12:03 +0330

    feat: import specific classes from data module
    - Import specific classes from the data module in __init__.py to provide direct access to key model components, enhancing modularity and code readability.

--- sage_meta/models/__init__.py
+++ sage_meta/models/__init__.py
@@ -1,1 +1,29 @@
-from .data import *
+from .data import (
+    Category,
+    Insight,
+    AccountInsight,
+    Comment,
+    Story,
+    Media,
+    InstagramAccount,
+    FacebookPageData,
+    UserData,
+    PostMention,
+    CommentMention,
+    ReplyMention,
+)
+
+__all__ = [
+    "Category",
+    "Insight",
+    "AccountInsight",
+    "Comment",
+    "Story",
+    "Media",
+    "InstagramAccount",
+    "FacebookPageData",
+    "UserData",
+    "PostMention",
+    "CommentMention",
+    "ReplyMention",
+]
